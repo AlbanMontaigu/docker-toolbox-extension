ed3bdc87a01a9677dd3c2e8b0c642791b8e53c34
Alban Montaigu <alban.montaigu@gmail.com>
2016-03-12 13:14:05 +0100

    No proxy in yum by default

--- iaas/provisioning/common/scripts/prepare-system.sh
+++ iaas/provisioning/common/scripts/prepare-system.sh
@@ -2,15 +2,15 @@
 #!/bin/sh
 
 # Proxy configuration if requested
-if grep -q PUT_YOUR_PROXY_HERE /etc/yum.conf; then
-    echo "== nothing todo with proxy in /etc/yum.conf !"
-else
-    echo 'proxy=http://PUT_YOUR_PROXY_HERE:3128' >> /etc/yum.conf
-    echo "== /etc/yum.conf updated with proxy configuration"
-fi
+#if grep -q your-proxy /etc/yum.conf; then
+#    echo "== nothing todo with proxy in /etc/yum.conf !"
+#else
+#    echo 'proxy=http://your-proxy:3128' >> /etc/yum.conf
+#    echo "== /etc/yum.conf updated with proxy configuration"
+#fi
 
 # Prepare requested folder for later configuration (see main.tf)
 mkdir /etc/systemd/system/docker.service.d
 
-# System global update
-yum -y update
+# System global update (uncomment to enable if requested)
+#yum -y update
