0729ced1b5039551b57f75da69c785c72e323545
Alban Montaigu <alban.montaigu@gmail.com>
2016-08-26 20:04:02 +0200

    Docker daemon start command fixed

--- iaas/provisioning/common/scripts/install-docker.sh
+++ iaas/provisioning/common/scripts/install-docker.sh
@@ -5,7 +5,7 @@ yum -y install docker-engine-1.12.1-1.el7.centos.x86_64
 
 # Docker installation
 sed -i -e "s%^\[Service\].*%\[Service\]\nEnvironmentFile=-/etc/sysconfig/docker%g" /usr/lib/systemd/system/docker.service
-sed -i -e "s%^ExecStart=.*%ExecStart=/usr/bin/docker daemon -H fd:// \$other_args%g" /usr/lib/systemd/system/docker.service
+sed -i -e "s%^ExecStart=.*%ExecStart=/usr/bin/dockerd -H unix:///var/run/docker.sock \$other_args%g" /usr/lib/systemd/system/docker.service
 
 # Restart to take in account the modifications
 systemctl daemon-reload
