c78fcd280d78dbf854fdc6ea5f7ca86fd896cc32
Alban Montaigu <alban.montaigu@gmail.com>
2017-03-10 13:44:02 +0100

    Script typo

--- iaas/provisioning/common/scripts/upgrade-kernel.sh
+++ iaas/provisioning/common/scripts/upgrade-kernel.sh
@@ -1,10 +1,9 @@
-
 #!/bin/sh
 
 #
 # Kernel upgrade to te last version to solve common issue:
 # @see https://github.com/docker/docker/issues/10294
-# So need to be kernel 3.18+ because centos7 at the 2017-02-24 is only 3.10.x
+# So need to be kernel 3.18+ because centos 7 at the 2017-02-24 is only 3.10.x
 #
 
 # export http_proxy=http://you_proxy_server:port
@@ -17,5 +16,5 @@ rpm  -Uvh http://www.elrepo.org/elrepo-release-7.0-2.el7.elrepo.noarch.rpm
 yum --enablerepo=elrepo-kernel install kernel-ml
 
 # Activate new kernel on host
-# Kernel sould be at least 4.10.x ont the 2017-02-24
+# Kernel should be at least 4.10.x ont the 2017-02-24
 grub2-set-default 0
